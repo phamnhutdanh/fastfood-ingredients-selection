bf1dca97cb70bc5258b8f601142699296f221e00
nhutdanh175 <nhutdanh175@gmail.com>
2023-10-20 14:28:11 +0700

    init app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+export default function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
