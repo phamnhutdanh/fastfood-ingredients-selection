e1636c64046d54986c25ec8ce359111268655c9b
nhutdanh175 <nhutdanh175@gmail.com>
2023-10-24 12:05:27 +0700

    Add Login screen and add event

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -5,29 +5,48 @@ import {BigTitleText} from '../components/texts/BigTitleText';
 import {GenericText} from '../components/texts/GenericText';
 import {TextLink} from '../components/texts/TextLink';
 import {StyleSheet} from 'react-native';
+import {useState} from 'react';
 
-export default function Login(): JSX.Element {
+export default function Login({navigation, route}: any): JSX.Element {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  /** @TODO login with email, password */
+  const onPressButtonLogin = () => {
+    console.log('email: ' + email);
+    console.log('password: ' + password);
+  };
+
+  const onPressSignUpLink = () => {
+    navigation.navigate('SignUp');
+  };
+
   return (
     <View style={styles.container}>
       <BigTitleText>Login</BigTitleText>
-      <Input label="Email" placeholder="Enter your email..."></Input>
+      <Input
+        label="Email"
+        placeholder="Enter your email..."
+        value={email}
+        onChangeText={setEmail}></Input>
       <Input
         label="Password"
         placeholder="Enter your password..."
-        secureTextEntry></Input>
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}></Input>
 
       <Button
         containerStyle={styles.button_containerStyle}
         buttonStyle={styles.button_buttonStyle}
-        titleStyle={styles.button_titleStyle}>
+        titleStyle={styles.button_titleStyle}
+        onPress={onPressButtonLogin}>
         LOGIN
       </Button>
 
       <View style={styles.textContainer}>
         <GenericText>Didn't have an account, </GenericText>
-        <TextLink onPressItem={() => console.log('navigate to sign up')}>
-          sign up
-        </TextLink>
+        <TextLink onPressItem={onPressSignUpLink}>sign up</TextLink>
       </View>
     </View>
   );
