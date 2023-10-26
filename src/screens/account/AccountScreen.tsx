39d087b22fa4f2caeb9582be16b5c6d6a7cbe935
nhutdanh175 <nhutdanh175@gmail.com>
2023-10-26 19:06:42 +0700

    refactor food and vendor details

--- src/screens/account/AccountScreen.tsx
+++ src/screens/account/AccountScreen.tsx
@@ -1,10 +1,19 @@
-import {Text} from '@rneui/themed';
+import {Avatar, Text} from '@rneui/themed';
 import {View} from 'react-native';
+import {TitleText} from '../../components/texts/TitleText';
+
+const avatarUri =
+  'https://static.vecteezy.com/system/resources/previews/005/857/332/non_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg';
 
 export default function AccountScreen(): JSX.Element {
   return (
     <View>
-      <Text>AccountScreen</Text>
+      <View>
+        <Avatar source={{uri: avatarUri}} size={90} rounded />
+        <TitleText>Smurf Cat</TitleText>
+      </View>
+
+      <View></View>
     </View>
   );
 }
