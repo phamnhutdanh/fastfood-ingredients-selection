693f5854a7fddbd936bed6df36c971c1af74ec87
nhutdanh175 <nhutdanh175@gmail.com>
2023-10-27 00:50:10 +0700

    add save cancel button components

--- src/screens/account/ChangePasswordScreen.tsx
+++ src/screens/account/ChangePasswordScreen.tsx
@@ -1,7 +1,8 @@
-import {Dimensions, StyleSheet, View} from 'react-native';
-import {Button, Input} from '@rneui/themed';
+import {StyleSheet, View} from 'react-native';
+import {Input} from '@rneui/themed';
 import colors from '../../styles/colors';
 import {useState} from 'react';
+import SaveCancelButton from './display/SaveCancelButton';
 
 type ThisProps = {
   navigation: any;
@@ -12,12 +13,6 @@ export default function ChangePasswordScreen(props: ThisProps): JSX.Element {
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [rePassword, setRePassword] = useState('');
-  const windowWidth = Dimensions.get('window').width;
-  const buttonWidth = (windowWidth - 40) / 2 - 10;
-
-  const goBack = () => {
-    props.navigation.goBack();
-  };
 
   const changePassword = () => {
     // if check current password && newPassword == rePassword
@@ -54,19 +49,10 @@ export default function ChangePasswordScreen(props: ThisProps): JSX.Element {
         onChangeText={setRePassword}
       />
 
-      <View style={styles.buttonContainer}>
-        <Button
-          titleStyle={styles.title}
-          buttonStyle={[styles.button, styles.cancel, {width: buttonWidth}]}
-          onPress={goBack}>
-          CANCEL
-        </Button>
-        <Button
-          buttonStyle={[styles.button, {width: buttonWidth}]}
-          onPress={changePassword}>
-          SAVE
-        </Button>
-      </View>
+      <SaveCancelButton
+        navigation={props.navigation}
+        onPressSave={changePassword}
+      />
     </View>
   );
 }
@@ -77,20 +63,6 @@ const styles = StyleSheet.create({
     gap: 20,
     paddingHorizontal: 20,
     paddingVertical: 32,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  button: {
-    paddingVertical: 12,
-    borderRadius: 20,
-  },
-  cancel: {
-    backgroundColor: colors.white,
-  },
-  title: {
-    color: colors.darkBlack,
   },
   input: {
     backgroundColor: colors.white,
---
+++ src/screens/account/display/SaveCancelButton.tsx
@@ -0,0 +1,55 @@
+import {Dimensions, StyleSheet, View} from 'react-native';
+import {Button} from '@rneui/themed';
+
+import {OnPressItem} from '../../../types/GenericType';
+import colors from '../../../styles/colors';
+
+type ThisProps = {
+  navigation: any;
+  onPressSave: OnPressItem;
+};
+
+export default function SaveCancelButton(props: ThisProps): JSX.Element {
+  const windowWidth = Dimensions.get('window').width;
+  const buttonWidth = (windowWidth - 40) / 2 - 10;
+
+  const goBack = () => {
+    props.navigation.goBack();
+  };
+
+  return (
+    <View style={styles.buttonContainer}>
+      <Button
+        titleStyle={styles.title}
+        buttonStyle={[styles.button, styles.cancel, {width: buttonWidth}]}
+        onPress={goBack}>
+        CANCEL
+      </Button>
+      <Button
+        buttonStyle={[styles.button, {width: buttonWidth}]}
+        onPress={props.onPressSave}>
+        SAVE
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  cancel: {
+    backgroundColor: colors.white,
+  },
+  title: {
+    color: colors.darkBlack,
+  },
+  input: {
+    backgroundColor: colors.white,
+  },
+});
